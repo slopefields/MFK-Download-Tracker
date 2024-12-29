640da9239d895109ff585b082ab0b2338ed28c76
slopefields <yooje44@gmail.com>
2024-12-28 18:11:56 -0800

    Fix script.js to take data.json

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-fetch('https://thunderstore.io/api/v1/package-metrics/Plotection/MoneyForKills')
+fetch('./data.json')
         .then(response => 
         {
             if(!response.ok)
                 throw new Error('Error with response: ${response.status}');
-            console.log('Response successful:', response);
             return response.json();
         })
         .then (data =>
         {
-            const downloadCount = data.downloads;
-            document.getElementById('downloads').textContent = 'Total Downloads: ${downloadCount}';
+            document.getElementById('downloads').textContent = 'Total Downloads: ${data.downloads}';
         })
         .catch(error => 
         {
