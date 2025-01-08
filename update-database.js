fc95aa4a1f0322ae8afbdb7588927080bd0c174e
slopefields <yooje44@gmail.com>
2025-01-07 20:50:20 -0800

    Add date fields alongside metrics for database

--- update-database.js
+++ update-database.js
@@ -14,14 +14,22 @@ const database = new sqlite3.Database(dbPath, err => {
         console.log('Successfully connected to modmetrics database.');
 });
 
+// Get current date
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = currentDate.getMonth() + 1; // Adding 1 because zero-indexed (Jan = 0, Sep = 8, etc.)
+const day = currentDate.getDate(); // getDay returns day of the week (like Monday) while getDate returns day of month (like 23)
+
+// Using serialize so that each step finishes before the next
 database.serialize(() =>
 {
     const insertQuery = 'INSERT OR IGNORE INTO metrics (year, month, day, downloads, rating_score, latest_version) VALUES (?, ?, ?, ?, ?, ?)';
-    
+
     jsonData.forEach((entry) =>
     {
         // Object destructuring: basically the same as const year = entry.year, const downloads = entry.downloads, etc.
-        const { year, month, day, downloads, rating_score, latest_version } = entry;
+        const { downloads, rating_score, latest_version } = entry;
+        // Take (year, month, day) from earlier, and (downloads, rating_score, and latest_version) from deconstruction.
         database.run(insertQuery, [year, month, day, downloads, rating_score, latest_version], err =>
         {
             if (err)
