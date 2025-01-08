ee12fad13812e15c3dd82bbcf2fbacee0d59479f
slopefields <yooje44@gmail.com>
2025-01-08 14:55:03 -0800

    Fix JSON object handling

--- update-database.js
+++ update-database.js
@@ -20,21 +20,19 @@ const year = currentDate.getFullYear();
 const month = currentDate.getMonth() + 1; // Adding 1 because zero-indexed (Jan = 0, Sep = 8, etc.)
 const day = currentDate.getDate(); // getDay returns day of the week (like Monday) while getDate returns day of month (like 23)
 
+// Object destructuring: basically the same as const year = jsonData.year, const downloads = jsonData.downloads, etc.
+const { downloads, rating_score, latest_version } = jsonData;
+
 // Using serialize so that each step finishes before the next
 database.serialize(() =>
 {
     const insertQuery = 'INSERT OR IGNORE INTO metrics (year, month, day, downloads, rating_score, latest_version) VALUES (?, ?, ?, ?, ?, ?)';
-
-    jsonData.forEach((entry) =>
+    
+    // Take (year, month, day) from earlier, and (downloads, rating_score, and latest_version) from deconstruction.
+    database.run(insertQuery, [year, month, day, downloads, rating_score, latest_version], err =>
     {
-        // Object destructuring: basically the same as const year = entry.year, const downloads = entry.downloads, etc.
-        const { downloads, rating_score, latest_version } = entry;
-        // Take (year, month, day) from earlier, and (downloads, rating_score, and latest_version) from deconstruction.
-        database.run(insertQuery, [year, month, day, downloads, rating_score, latest_version], err =>
-        {
-            if (err)
-                console.error('Error inserting data', err.message);
-        });
+        if (err)
+            console.error('Error inserting data', err.message);
     });
 });
 
