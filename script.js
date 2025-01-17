0ff0f4e514b29472e4830bdad66b4b1c9d2b66d0
slopefields <yooje44@gmail.com>
2025-01-16 22:51:07 -0800

    Add comments throughout for general explanation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Open connection to modmetrics.db
 const sqlite3 = require('sqlite3');
 const dbPath = './modmetrics.db';
 const database = new sqlite3.Database(dbPath, data =>
@@ -8,7 +9,9 @@ const database = new sqlite3.Database(dbPath, data =>
         console.log('Successfully connected to modmetrics database in script.js.');
 });
 
+// Retrieve data.json file
 fetch('./data.json')
+        // Handle HTTP-level errors like 404 Not Found or 500 Server Error
         .then(response => 
         {
             if(!response.ok)
@@ -22,13 +25,17 @@ fetch('./data.json')
             const downloadsElement = document.getElementById('downloads');
             downloadsElement.textContent = `Total Downloads: ${data.downloads}`;
             
+            // Make text + or - based on increase or decrease
             const differenceText = difference >= 0 ? ` (+${difference} compared to yesterday)` : ` (-${difference} compared to yesterday)`;
             const tempDifference = document.createElement('difference');
             tempDifference.textContent = differenceText;
+            //  Change color based on increase or decrease
             tempDifference.style.color = difference >= 0 ? 'green' : 'red';
             
+            // Append colored text to the rest of text (Total Downloads: ...)
             downloadsElement.appendChild(tempDifference);
         })
+        // Final safety net
         .catch(error => 
         {
             console.error('Error fetching data:', error);
